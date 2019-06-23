ac803b55d9cd37d4a9a9edbc6d05433e1536b077
cat <itcat@123.com>
2019-06-23 16:02:37 +0800

    Initial commit!!!

---
+++ dubboxdemo_utils/src/main/java/cn/itcast/dubbodemo/service/UserService.java
@@ -0,0 +1,5 @@
+package cn.itcast.dubbodemo.service;
+
+public interface UserService {
+    public String getName();
+}
---
+++ dubboxdemoweb/src/main/java/cn/itcast/dubboxdemo/controller.java
@@ -0,0 +1,25 @@
+package cn.itcast.dubboxdemo;
+
+
+import cn.itcast.dubbodemo.service.UserService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+
+public class controller {
+
+    @Reference
+    private UserService userService;
+
+
+    @RequestMapping("/showName")
+    @ResponseBody
+    public String showName(){
+        return userService.getName();
+    }
+}
+
